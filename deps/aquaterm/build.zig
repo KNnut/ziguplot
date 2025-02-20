e637cbf870d68720f457b08ad8a7dc768fe0c4dc
KNnut <9387720+KNnut@users.noreply.github.com>
2025-02-20 13:41:09 +0800

    build: update to zig 0.14.0-dev.3267+59dc15fa0

--- deps/aquaterm/build.zig
+++ deps/aquaterm/build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
-    if (!target.result.isDarwin()) {
+    if (!target.result.os.tag.isDarwin()) {
         std.debug.print("The target should be macOS but is {s}.\n", .{@tagName(target.result.os.tag)});
         return error.InvalidOS;
     }
